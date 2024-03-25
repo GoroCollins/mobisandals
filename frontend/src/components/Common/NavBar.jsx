6a2353515821e78620c4ba7bb5e2953a9b7fe562
Goro Collins <gorocollins@gmail.com>
2024-03-25 20:11:49 +0300

    Adjusted the logo size

--- frontend/src/components/Common/NavBar.jsx
+++ frontend/src/components/Common/NavBar.jsx
@@ -3,7 +3,7 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import { LinkContainer } from "react-router-bootstrap";
-import logo from "assets/logo.jpeg"
+import logo from "../../assets/logo.jpeg"  
 
 export default function NavBar() {
   return (
@@ -11,7 +11,7 @@ export default function NavBar() {
     data-bs-theme="dark">
       <Container>
       <LinkContainer className="link-primary" to="/">
-            <Navbar.Brand><img src={logo} alt="Mobi Sandals"/></Navbar.Brand>
+            <Navbar.Brand><img src={logo} alt="Mobi Sandals" height={50} width={50}/></Navbar.Brand>
           </LinkContainer>
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
