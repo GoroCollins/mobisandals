bb45df424e5aafe9e973e6505076da70b6bc148e
Goro Collins <gorocollins@gmail.com>
2024-03-28 20:24:52 +0300

    Updated the url for categories and add testing for the endpoint of shoes

--- backend/products/tests/test_urls.py
+++ backend/products/tests/test_urls.py
@@ -2,8 +2,14 @@ from django.test import TestCase
 
 class UrlTest(TestCase):
 
-    def testHomePage(self):
-        response = self.client.get('/categories')
+    def testCategoriesPage(self):
+        response = self.client.get('/productscategories/')
+        print(response)
+
+        self.assertEqual(response.status_code, 200)
+        
+    def testShoesPage(self):
+        response = self.client.get('/productsshoes/')
         print(response)
 
         self.assertEqual(response.status_code, 200)
