eb6ad7b4da657de12f8267b6c3a2b4720e2bd4fc
Goro Collins <52543602+GoroCollins@users.noreply.github.com>
2024-03-30 10:09:43 +0000

    Updated the test to test for image

--- frontend/src/tests/NavBar.test.jsx
+++ frontend/src/tests/NavBar.test.jsx
@@ -1,24 +1,33 @@
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import userEvent from '@testing-library/user-event';
-import { BrowserRouter as Router } from 'react-router-dom'; // Import BrowserRouter
+import { BrowserRouter as Router } from 'react-router-dom';
 import NavBar from '../components/Common/NavBar';
-
+import logo from '../assets/logo.jpeg'; // Import the logo image
 
 describe('NavBar', () => {
-  test('renders navbar with brand name', () => {
+  test('renders navbar with brand name and logo', () => {
     render(
-      <Router> {/* Wrap NavBar with Router component */}
+      <Router>
         <NavBar />
       </Router>
     );
-    const brandElement = screen.getByText('Awesome Shop');
+
+    // Check for brand name
+    const brandElement = screen.getByText('Mobi Sandals');
     expect(brandElement).toBeInTheDocument();
+
+    // Check for logo image
+    const logoElement = screen.getByAltText('Mobi Sandals');
+    expect(logoElement).toBeInTheDocument();
+    expect(logoElement).toHaveAttribute('src', logo); // Check src attribute matches imported logo
+    expect(logoElement).toHaveAttribute('height', '50');
+    expect(logoElement).toHaveAttribute('width', '50');
   });
 
   test('navigates to home page when home link is clicked', () => {
     render(
-      <Router> {/* Wrap NavBar with Router component */}
+      <Router>
         <NavBar />
       </Router>
     );
